54e00a22decc3eb535844a01a0e2e22ed5847d45
Kenji Emura <kenjiemura@gmail.com>
2021-07-27 23:12:24 +0900

    Started creating a basic quiz app

--- playground/flutter_complete_guide/lib/main.dart
+++ playground/flutter_complete_guide/lib/main.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyCoolApp());
-}
+// void main() {
+//   runApp(MyCoolApp());
+// }
+
+void main() => runApp(MyCoolApp());
 
 class MyCoolApp extends StatelessWidget {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    questionIndex++;
+    print(questionIndex);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What's your favorite color?",
+      "What's your favorite animal?",
+    ];
     return MaterialApp(
-      home: Text("Kenji's Amazing App"),
+      // home: Text("Kenji's Amazing App!"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            ElevatedButton(onPressed: answerQuestion, child: Text('Answer 1')),
+            ElevatedButton(onPressed: () {}, child: Text('Answer 2')),
+            ElevatedButton(onPressed: () {}, child: Text('Answer 3'))
+          ],
+        ),
+      ),
     );
   }
 }
