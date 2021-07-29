cf432e0157e7a1656b7fd448d5ee4bb4390b9192
Kenji Emura <kenjiemura@gmail.com>
2021-07-29 13:50:57 +0900

    The app now doesn't crash when question's index is exceeded

--- playground/flutter_complete_guide/lib/main.dart
+++ playground/flutter_complete_guide/lib/main.dart
@@ -3,10 +3,6 @@ import 'package:flutter/material.dart';
 import 'question.dart';
 import 'answer.dart';
 
-// void main() {
-//   runApp(MyApp());
-// }
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
@@ -16,45 +12,52 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
+  final questions = [
+    {
+      'question': "What's your favorite color?",
+      'answers': ['Black', 'Red', "Green", 'White']
+    },
+    {
+      'question': "What's your favorite animal?",
+      'answers': ['Rabbit', 'Snake', "Elephant", 'Lion']
+    },
+    {
+      'question': "Who's your favorite instructor?",
+      'answers': ['Max', 'Kenji', "Hernando", 'Kaori']
+    },
+  ];
 
   void _answerQuestion() {
     setState(() {
       _questionIndex++;
     });
-    print(_questionIndex);
+    if (_questionIndex < questions.length) {
+      print("We still have more questions to display!");
+    } else {
+      print('No more questions avalaible!');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'question': "What's your favorite color?",
-        'answers': ['Black', 'Red', "Green", 'White']
-      },
-      {
-        'question': "What's your favorite animal?",
-        'answers': ['Rabbit', 'Snake', "Elephant", 'Lion']
-      },
-      {
-        'question': "Who's your favorite instructor?",
-        'answers': ['Max', 'Kenji', "Hernando", 'Kaori']
-      },
-    ];
-
     return MaterialApp(
       // home: Text("Kenji's Amazing App!"),
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Column(
-          children: [
-            Question(questions[_questionIndex]['question']),
-            ...(questions[_questionIndex]['answers'] as List<String>)
-                .map((answer) => Answer(_answerQuestion, answer))
-                .toList()
-          ],
-        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: [
+                  Question(questions[_questionIndex]['question']),
+                  ...(questions[_questionIndex]['answers'] as List<String>)
+                      .map((answer) => Answer(_answerQuestion, answer))
+                      .toList()
+                ],
+              )
+            : Center(
+                child: Text("You did it!"),
+              ),
       ),
     );
   }
