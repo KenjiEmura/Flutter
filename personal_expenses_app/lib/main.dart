8537bb412087a740ff4d7b0235cefd2ad928485b
Kenji Emura <kenjiemura@gmail.com>
2021-07-30 13:36:48 +0900

    Created the 'transaction' class

--- personal_expenses_app/lib/main.dart
+++ personal_expenses_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Expenses Tracker',
+      title: "Kenji's Expenses Tracker",
       home: MyHomePage(),
       debugShowCheckedModeBanner: false,
     );
@@ -14,14 +15,43 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', title: "Kenji's shoes", amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Green Tea', amount: 1.29, date: DateTime.now()),
+    Transaction(
+        id: 't3', title: 'Romantic Dinner', amount: 35.2, date: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Expenses Tracker'),
+        title: Text("Kenji's Expenses Tracker"),
       ),
       body: Center(
-        child: Text('Widget Playground!'),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('CHART'),
+                elevation: 5,
+              ),
+            ),
+            Column(
+              children: [
+                ...transactions
+                    .map((transaction) => Card(child: Text(transaction.title)))
+                    .toList(),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
